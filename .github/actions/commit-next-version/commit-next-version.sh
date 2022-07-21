819e6142b850b0934140a7de1dc3e48bbf21aeec
George Isaev <georgeisaev@gmail.com>
2022-07-21 17:47:33 +0400

    gh

--- .github/actions/commit-next-version/commit-next-version.sh
+++ .github/actions/commit-next-version/commit-next-version.sh
@@ -37,8 +37,7 @@ main() {
   which gh
 
   gh pr create --base "$BRANCH_NAME" --head "$release_branch" --title "$tag" --body "$tag"
-  gh pr review --approve
-  gh pr merge --admin --rebase --delete-branch
+  gh pr merge --admin --body "$tag"
 
   # push tag
   git tag -a "$tag" -m "$tag"
