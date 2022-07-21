22178183fd473843e2b25b8a5d0bbfd7256b917d
George Isaev <georgeisaev@gmail.com>
2022-07-21 17:12:52 +0400

    gh

--- .github/actions/commit-next-version/commit-next-version.sh
+++ .github/actions/commit-next-version/commit-next-version.sh
@@ -35,7 +35,8 @@ main() {
   git push -u origin "$release_branch"
 
   which gh
-  gh auth login --with-token "${GITHUB_TOKEN}"
+
+  gh auth login --with-token < "${GITHUB_TOKEN}"
   gh status
 
   gh pr create --base "$BRANCH_NAME" --head "$release_branch"
