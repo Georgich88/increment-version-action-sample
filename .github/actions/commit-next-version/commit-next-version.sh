ab990f1b0385d4b73956718f2530cb037d50c5e3
George Isaev <georgeisaev@gmail.com>
2022-07-21 15:46:14 +0400

    gh

--- .github/actions/commit-next-version/commit-next-version.sh
+++ .github/actions/commit-next-version/commit-next-version.sh
@@ -32,6 +32,7 @@ main() {
 
   # push new version
   git push -u origin "$release_branch"
+  man gh
   pr_url=$(gh pr create --title "$tag" --base "$BRANCH_NAME" --head "$release_branch")
   gh pr review "$pr_url" --approve
   gh pr merge "$pr_url" --admin --rebase --delete-branch
